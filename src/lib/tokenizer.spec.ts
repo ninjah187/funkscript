52b58210f364cc3730df1b8eea4b66caa7bad466
Karol Hinz <karol@hinz.tech>
2023-03-28 21:20:06 +0200

    feat: add simple function compiling (#3)
    Add simple one-liner function compiling. Also, all variable declarations are treated now as a
    function declarations.

--- src/lib/tokenizer.spec.ts
+++ src/lib/tokenizer.spec.ts
@@ -3,7 +3,7 @@
 
 import test from 'ava';
 
-import { TokenType, token, tokenize } from './tokenizer';
+import { Token, TokenType, token, tokenize } from './tokenizer';
 
 test('tokenizer can tokenize simple assignment', t => {
   const fs = 'let x = 5';
@@ -13,6 +13,7 @@ test('tokenizer can tokenize simple assignment', t => {
     token('x', TokenType.symbol),
     token('=', TokenType.equal),
     token('5', TokenType.literal),
+    endOfLine()
   ];
   
   const tokens = tokenize(fs);
@@ -21,11 +22,6 @@ test('tokenizer can tokenize simple assignment', t => {
 });
 
 test('tokenizer can tokenize multi lines', t => {
-  // const fs = `
-  //   let a = 2
-  //   let b = 3
-  // `;
-
   const fs =
     'let a = 2\n' +
     'let b = 3';
@@ -35,14 +31,61 @@ test('tokenizer can tokenize multi lines', t => {
     token('a', TokenType.symbol),
     token('=', TokenType.equal),
     token('2', TokenType.literal),
-    // eol token here ??
+    endOfLine(),
     token('let', TokenType.declaration),
     token('b', TokenType.symbol),
     token('=', TokenType.equal),
     token('3', TokenType.literal),
+    endOfLine(),
   ];
 
   const tokens = tokenize(fs);
 
   t.deepEqual(tokens, expected);
 });
+
+test('tokenizer can tokenize function declaration: one-liner with single parameter', t => {
+  const fs = 'let f x = x + 1';
+
+  const expected = [
+    token('let', TokenType.declaration),
+    token('f', TokenType.symbol),
+    token('x', TokenType.symbol),
+    token('=', TokenType.equal),
+    token('x', TokenType.symbol),
+    token('+', TokenType.plus),
+    token('1', TokenType.literal),
+    endOfLine(),
+  ];
+
+  const tokens = tokenize(fs);
+
+  t.deepEqual(tokens, expected);
+});
+
+test('tokenizer can tokenize function declaration: one-liner with two parameters', t => {
+  const fs = 'let sum a b = a + b';
+
+  const expected = [
+    token('let', TokenType.declaration),
+    token('sum', TokenType.symbol),
+    token('a', TokenType.symbol),
+    token('b', TokenType.symbol),
+    token('=', TokenType.equal),
+    token('a', TokenType.symbol),
+    token('+', TokenType.plus),
+    token('b', TokenType.symbol),
+    endOfLine(),
+  ];
+
+  const tokens = tokenize(fs);
+
+  t.deepEqual(tokens, expected);
+});
+
+function endOfLine(): Token {
+  return {
+    type: TokenType.endOfLine,
+    value: '\n'
+  };
+}
